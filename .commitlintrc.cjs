9fb323cd3db8d19a7f7a59a4f39637e896f59f59
Christophe G <1050197+cgaube@users.noreply.github.com>
2022-04-03 16:32:26 -0700

    chore(commitlint): increase body-max-line-length to please dependabot

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -5,4 +5,10 @@
  */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  // Override rules
+  rules: {
+    // Allow body lines to be more than 100 characters as dependabot messages
+    // are quite verbose
+    'body-max-line-length': [2, 'always', '255'],
+  },
 }
