2f0373280fe322516a561092d0fd8e1ff3eb9649
Christophe Gaube <1050197+cgaube@users.noreply.github.com>
2024-12-02 09:49:03 -0800

    bug fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig((options) => ({
+export default defineConfig(() => ({
   clean: true,
   entry: ['src/index.ts', 'src/dev.ts'],
   splitting: true,
   sourcemap: false,
-  minify: !options.watch,
+  minify: false, // Don't minify so Awilix can work.
   format: ['esm'],
   outDir: 'dist',
   dts: true,
